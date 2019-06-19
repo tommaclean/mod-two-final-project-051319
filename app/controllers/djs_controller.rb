517853330a34e5c6c8ff55b50fe5c1fc91c30f2b
Tess Neau <tessneau@gmail.com>
2019-06-19 15:35:34 -0400

    most views are up

--- app/controllers/djs_controller.rb
+++ app/controllers/djs_controller.rb
@@ -14,8 +14,29 @@ class DjsController < ApplicationController
     end
   end
 
+  def edit
+    @dj = Dj.find(params[:id])
+  end
+
+  def update
+    @dj = Dj.find(params[:id])
+    @dj.update(dj_params(:username, :name, :bio))
+    if @dj.valid?
+      redirect_to @dj
+    else
+      flash[:errors] = @dj.errors.full_messages
+      redirect_to edit_dj_path
+    end
+  end
+
   def show
     @dj = Dj.find(params[:id])
+  end
+
+  def destroy
+    @dj = Dj.find(params[:id])
+    @dj.destroy
+    redirect_to login_path
   end
 
   private
--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,2 +1,54 @@
 class PlaylistsController < ApplicationController
+  before_action :find_playlist, only: [:show, :edit, :update, :destroy]
+
+  def show
+
+  end
+
+  def new
+    @playlist = Playlist.new
+  end
+
+  def create
+    @dj =
+    @playlist = Playlist.create(playlist_params(:name, :description, dj_id: @current_user.id))
+    if @playlist.valid?
+      redirect_to @playlist
+    else
+      flash[:errors] = @playlist.errors.full_messages
+      redirect_to new_playlist_path
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+    @playlist.update(dj_params(:name, :description))
+    if @playlist.valid?
+      redirect_to @playlist
+    else
+      flash[:errors] = @playlist.errors.full_messages
+      redirect_to edit_playlist_path
+    end
+  end
+
+  def destroy
+    @dj = @playlist.dj
+    @playlist.destroy
+    redirect_to @dj
+  end
+
+  private
+
+  def find_playlist
+    @playlist = Playlist.find(params[:id])
+  end
+
+  def playlist_params(*args)
+    params.require(:dj).permit(*args)
+  end
+
+
 end
