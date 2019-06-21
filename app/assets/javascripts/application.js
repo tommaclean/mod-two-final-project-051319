dff0f35ba87eece7d2863c04dc877d76a8c61aa1
tommaclean <tom.j.maclean@gmail.com>
2019-06-21 12:50:30 -0400

    search page functionality

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,3 +18,16 @@
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+$(document).ready(function() {
+    var audioSection = $('section#song');
+    $('a.play_song').click(function() {
+
+    var audio = $('<audio>', {
+        controls : 'controls'
+    });
+
+    var url = $(this).attr('href');
+    $('<source>').attr('src', url).appendTo(audio);
+    audioSection.html(audio);
+    return false;       });
+}); 
